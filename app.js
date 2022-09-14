177315e56b9931fe596852ff8920717197ee116a
gary909 <garywhite61@hotmail.com>
2022-09-14 14:11:04 +0200

    code working

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 function rowSumOddNumbers(n) {
-    // TODO
+    // let startingNum = (n * n) - (n - 1); // left hand side number
+    // let ans = 0;
+    // if (n > 1) {
+
+    //     for (let i = 0; i < n; i++) {
+    //         ans = ans + 1;
+    //     }
+    // }
+
+    // return ans;
+    return n * n * n;
 }
 
 console.log(rowSumOddNumbers(1)); // 1
+console.log(rowSumOddNumbers(4)); // 64
+console.log(rowSumOddNumbers(5)); // 125
 console.log(rowSumOddNumbers(42)); // 74088
